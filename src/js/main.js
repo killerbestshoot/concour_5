1a1cfdb9a5141f498e5649a1e16638fea7617f6e
killerbestshoot <killerbestshoot@gmail.com>
2023-03-24 00:52:48 -0400

    troisemme commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,16 @@
 const butt_name = document.getElementById('butt_1_name');
 console.log(butt_name)
 var i = 0;
-// var story ='d';
 var speed = 50;
 
     function typeWriter(txt,container) {
-        var texte=txt;
-        console.log(texte.length)
-        if (i < texte.length) {
+        if (i < txt.length) {
             document.getElementById(container).innerHTML += txt.charAt(i);
             console.log(txt);
             i++;
-
             setTimeout(typeWriter, speed);
         }
     }
-    // butt_name.addEventListener()
-
-// console.log('bnj')
-var t ='le jur';
-// console.log(t.)
 butt_name.addEventListener('click',()=>{
     var name=document.getElementById('name').value;
     typeWriter(name,'username');
