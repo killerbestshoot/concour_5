4be3b0319dcc152a5a66b82cc77ef1928a8ca198
killerbestshoot <killerbestshoot@gmail.com>
2023-03-26 13:45:26 -0400

    septiemme commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '25px',
+      '8': '35px',
+    },
     extend: {},
   },
   plugins: [],
